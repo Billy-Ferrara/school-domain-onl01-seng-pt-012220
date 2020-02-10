656d3310eb1b1b07e595cbb846a98cd8f66c8143
Billy Ferrara <willfira717@gmail.com>
2020-02-10 20:40:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,18 @@
-class School
+ def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
 
-def initialize(name)
-  @name = name
-  @roster = {}
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
 end
-
-def roster 
-  @roster
-end
-
-def add_student(name, grade)
-  
